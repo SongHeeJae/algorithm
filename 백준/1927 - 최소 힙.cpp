2fbcf4f24eec8d365f2b6dc10ce09abf5e853411
SongHeeJae <gmlwo308@gmail.com>
2021-04-04 20:36:53 +0900

    백준 1927 : 최소 힙

---
+++ 백준/1927 - 최소 힙.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n, a;
+	priority_queue<int> pq;
+	scanf("%d", &n);
+	while (n--) {
+		scanf("%d", &a);
+		if (a == 0) {
+			if (pq.empty()) printf("0\n");
+			else {
+				printf("%d\n", -pq.top());
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(-a);
+		}
+	}
+}
