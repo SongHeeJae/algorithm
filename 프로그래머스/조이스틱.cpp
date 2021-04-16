9801572eaa00d02dba6c0834a7d9e999ad2c67fd
SongHeeJae <gmlwo308@gmail.com>
2021-04-17 04:08:40 +0900

    프로그래머스 : 조이스틱(코드 수정)

--- 프로그래머스/조이스틱.cpp
+++ 프로그래머스/조이스틱.cpp
@@ -8,17 +8,14 @@ int moveNext(int cur, bool right, int len) {
 }
 
 int solution(string name) {
-    bool visit[20] = {false};
     int cnt[26] = {0}, cur = 0, len = name.size(), answer = 0;
     for(int i=0; i<26; i++) cnt[i] = min(i, 26 - i);  
     while(1) {
         bool right = true;
         int dist = 0;
         for(; dist<len; dist++) {
-            int idx1 = (cur + dist) % len;
-            int idx2 = (cur - dist + len) % len;
-            if(!visit[idx1] && name[idx1] != 'A') break;
-            if(!visit[idx2] && name[idx2] != 'A') {
+            if(name[(cur + dist) % len] != 'A') break;
+            if(name[(cur - dist + len) % len] != 'A') {
                 right = false;
                 break;
             }
@@ -26,10 +23,8 @@ int solution(string name) {
         if(dist == len) break;
         answer += dist;
         while(1) {
-            if(!visit[cur]) {
-                answer += cnt[name[cur] - 'A'];
-                visit[cur] = true;
-            }
+            answer += cnt[name[cur] - 'A'];
+            name[cur] = 'A';
             if(dist-- == 0) break;
             cur = moveNext(cur, right, len);
         }
@@ -41,4 +36,5 @@ int solution(string name) {
 먼저 각 알파벳을 뽑을 수 있는 최소 횟수를 초기화 해줍니다.
 그 후, 현재 위치에서 아직 값을 변경 안했으면서 'A'가 아닌 가장 가까운 위치를 찾아줍니다.
 해당 위치로 커서를 이동하면서 조작 횟수를 업데이트해줍니다.
+업데이트한 위치는 'A'로 값을 변경해서 방문 표시해줍니다.
 */
