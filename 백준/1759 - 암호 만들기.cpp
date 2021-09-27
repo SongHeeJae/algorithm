bcda7d09711171ee6059ced1af4f774efffe7af2
SongHeeJae <gmlwo308@gmail.com>
2021-09-27 12:34:19 +0900

    백준 1759 : 암호 만들기

--- 백준/1759 - 암호 만들기.cpp
+++ 백준/1759 - 암호 만들기.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+#define MAX 16
+
+int l, c;
+char ch[MAX], res[MAX];
+
+bool isMo(char alp) {
+	return alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u';
+}
+
+bool isJa(char alp) {
+	return !isMo(alp);
+}
+
+void f(int idx, int mo, int ja, int len) {
+	if (idx == c + 1) return;
+	if (len == l) {
+		if (mo > 0 && ja > 1) {
+			cout << res << "\n";
+		}
+		return;
+	}
+
+	res[len] = ch[idx];
+	f(idx + 1, mo + isMo(ch[idx]), ja + isJa(ch[idx]), len + 1);
+	f(idx + 1, mo, ja, len);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> l >> c;
+	for (int i = 0; i < c; i++) {
+		cin >> ch[i];
+	}
+	sort(ch, ch + c);
+	f(0, 0, 0, 0);
+ }
+
+ /*
+ 주어진 문자를 정렬하고, 재귀로 가능성 있는 암호문을 구해주었습니다.
+
+ --- 기존 코드 ---
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int l, c;
 char a[15], b[16];
 bool visited[26] = { false, };
@@ -35,3 +82,4 @@ int main() {
 	sort(a, a + c);
 	dfs(0, 0);
 }
+ */
