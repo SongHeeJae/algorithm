4f0bb4eac441d5f7ca046d0ad98a5397c722daaa
SongHeeJae <gmlwo308@gmail.com>
2023-02-20 21:48:55 +0900

    LeetCode 35 - Search Insert Position

---
+++ LeetCode/35 - Search Insert Position.cpp
@@ -0,0 +1,19 @@
+/*
+lower bound 문제였습니다.
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r) {
+            int m = (l + r) / 2;
+            if(nums[m] >= target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+};
