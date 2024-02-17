6695c5a85f01c763fb382704c41ed15ebe693ab8
SongHeeJae <gmlwo308@gmail.com>
2024-02-17 16:33:47 +0900

    LeetCode 1481. Least Number of Unique Integers after K Removals

--- LeetCode/1481. Least Number of Unique Integers after K Removals.cpp
+++ LeetCode/1481. Least Number of Unique Integers after K Removals.cpp
@@ -20,3 +20,30 @@ public:
         return 0;
     }
 };
+
+/*
+arr.length <= 10^5 이기 때문에, 각 빈도 수에 등장하는 고유한 숫자의 개수를, 상수 시간에 접근할 수 있도록 배열을 이용하여 초기화주면, 별도 정렬 없이 선형 시간에 결과를 구할 수 있습니다.  
+*/
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> numToCntMap;
+        for(int num : arr) numToCntMap[num]++;
+
+        vector<int> cnt(arr.size() + 1, 0);
+        for(auto it : numToCntMap) cnt[it.second]++;
+
+        int c = 0;
+        for(int i=1; i<cnt.size(); i++) {            
+            while(cnt[i]--) {
+                k -= i;
+                c++;
+                if(k < 0) return numToCntMap.size() - c + 1;
+            }
+        }
+        return 0;
+    }
+};
+
+
